d1eb6548eeafa4e0db95c5a34643f1a27d61bac9
Diogo22001907 <diogo.mv.ferreira19@outlook.pt>
2023-03-23 17:12:35 +0000

    Exercise 6 completed

--- Power2Method/Program.cs
+++ Power2Method/Program.cs
@@ -11,6 +11,7 @@ namespace Power2Method
         /// <summary>
         /// Calculates the power of 2 up to 5.
         /// </summary>
+        
         private static void PowersOf2Until5()
         {
             for (int i = 1; i <= (1 << 5); i = i << 1)
@@ -19,11 +20,13 @@ namespace Power2Method
                 Console.WriteLine(i);
             }
         }
+        
 
         /// <summary>
         /// Calculates the powers of 2 up to specified number
         /// </summary>
         /// <param name="n">The maximum power of 2 to calculate.</param>
+        
         private static void PowerOf2UntilN(int n)
         {
             for (int i = 1; i <= (1 << n); i = i << 1)
@@ -31,6 +34,22 @@ namespace Power2Method
                 Console.WriteLine(i);
             }
         }
+        
+
+        /// <summary>
+        /// This method calculates the powers of two between the n1 and n2 values.
+        /// </summary>
+        /// <param name="n1">The first number that it will display the value of the power 2</param>
+        /// <param name="n2">The last number that it will display the value of the power 2</param>
+        private static void PowerOf2UntilN(int n1, int n2)
+        {
+            for (int i = n1; i <= (n1 << n2); i = i << 1)
+            {
+                Console.WriteLine(i);
+            }
+        }
+
+        
 
         /// <summary>
         /// The method PowersOf2Until5() is called 2 times to output the values of the power of 2 up to 5
@@ -39,7 +58,7 @@ namespace Power2Method
         /// <param name="args"></param>
         static void Main(string[] args)
         {
-
+            
             PowerOf2UntilN(2);
             Console.WriteLine("------------------------");
             PowerOf2UntilN(3);
@@ -47,6 +66,8 @@ namespace Power2Method
             PowersOf2Until5();
             Console.WriteLine("------------------------");
             PowersOf2Until5();
+            Console.WriteLine("------------------------");
+            PowerOf2UntilN(2,4);
 
         }
     }
